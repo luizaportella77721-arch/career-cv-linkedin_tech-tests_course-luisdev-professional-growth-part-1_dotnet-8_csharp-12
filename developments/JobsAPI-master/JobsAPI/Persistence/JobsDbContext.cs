697b8dde95770a62c18a8908739f8e9c43098e00
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-23 13:52:53 -0300

    feat: adição do arquivo JobsDbContext.cs

---
+++ developments/JobsAPI-master/JobsAPI/Persistence/JobsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using JobsAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobsAPI.Persistence
+{
+    public class JobsDbContext : DbContext
+    {
+        public JobsDbContext(DbContextOptions<JobsDbContext> options) : base(options)
+        { }
+
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // NÃO ESQUECER DE DEFINIR CHAVE PRIMÁRIA
+            builder.Entity<Job>(o =>
+            {
+                // o.Property(j => j.Title).HasMaxLength(25);
+                o.HasKey(j => j.Id);
+            });
+        }
+    }
+}
